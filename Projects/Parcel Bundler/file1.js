fd6beba7f72912b5d72260c83c899ea8dd7c4e4b
Ayan Khan <impracticalayan@gmail.com>
2023-11-02 09:15:06 +0530

    Parcel builder

---
+++ Projects/Parcel Bundler/file1.js
@@ -0,0 +1,6 @@
+const header1 = document.querySelector('.class-1')
+const header2 = document.querySelector('.class-2')
+
+header1.innerHTML = "Changed Heading 1"
+
+header2.style.backgroundColor = 'red'
---
+++ Projects/Parcel Bundler/file3.js
@@ -0,0 +1,7 @@
+const $f5e572869b585825$var$header1 = document.querySelector(".class-1");
+const $f5e572869b585825$var$header2 = document.querySelector(".class-2");
+$f5e572869b585825$var$header1.innerHTML = "Changed Heading 1";
+$f5e572869b585825$var$header2.style.backgroundColor = "red";
+
+
+//# sourceMappingURL=file3.js.map
