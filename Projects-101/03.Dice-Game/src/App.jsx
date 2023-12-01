4a3e434c521d1ddf51d5ee6ed094b70e8d020cdd
Ayan Khan <impracticalayan@gmail.com>
2023-12-01 14:06:59 +0530

    Dicgame:WIP

---
+++ Projects-101/03.Dice-Game/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LandingPage from "./LandingPage/LandingPage";
+
+const App = () => {
+  return <React.StrictMode>
+    <div className="container">
+    <LandingPage/>
+    </div>
+  </React.StrictMode>
+}
+
+export default App;
---
+++ Projects-101/03.Dice-Game/src/LandingPage/LandingPage.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-component'
+
+const LandingPage  = () => {
+    return <h1>Landing page</h1>
+}
+
+export default LandingPage;
+
+const Container = styled.div
