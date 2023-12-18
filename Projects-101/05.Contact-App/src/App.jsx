3de0a52ed10604939a60a1108290eeee2929bcd5
Ayan Khan <impracticalayan@gmail.com>
2023-12-18 12:21:06 +0530

    firebase-contact-project completed

--- Projects-101/05.Contact-App/src/App.jsx
+++ Projects-101/05.Contact-App/src/App.jsx
@@ -1,56 +1,84 @@
 import Navbar from "./components/Navbar";
 import {FiSearch} from 'react-icons/fi'
-import {AiFillPlusCircle} from 'react-icons/ai'
-import {collection,getDocs} from 'firebase/firestore'
-import {db} from './config/firebase'
 import { useEffect, useState } from "react";
-import {HiOutlineUserCircle} from 'react-icons/hi'
-import {IoMdTrash} from 'react-icons/io'
-import {RiEditCircleLine} from 'react-icons/ri'
+import {AiFillPlusCircle} from 'react-icons/ai'
+import {collection,getDocs, onSnapshot} from 'firebase/firestore'
+import {db} from './config/firebase'
 import ContactCard from "./components/ContactCard";
+import Modal from "./components/Modal";
+import AddAndUpdateContact from "./AddAndUpdateContact";
+import useDisclouse from "./hooks/useDisclouse";
 
 
 function App() {
   const [contacts,setContacts] = useState([])
+  const {isOpen,onClose,onOpen} = useDisclouse()
+
+
 
   useEffect(()=>{
     const getContacts = async () => {
       try {
         const contactsRef = collection(db,'contacts')
-        const contactSnapshot = await getDocs(contactsRef)
-        const contactLists = contactSnapshot.docs.map((doc)=>{
-          return {
-            id:doc.id,
-            ...doc.data()
-          }
+        onSnapshot(contactsRef,(snapshot)=>{
+          const contactLists = snapshot.docs.map((doc)=>{
+            return{
+              id:doc.id,
+              ...doc.data()
+            }
+          })
+          setContacts(contactLists)
+          return contactLists
         })
-        setContacts(contactLists)
       } catch (error) {
-        
+        console.log(error)
       }
     }
     getContacts()
   },[])
 
+  const filterContacts = (e) =>{
+    const value = e.target.value
+    const contactsRef = collection(db,'contacts')
+    onSnapshot(contactsRef,(snapshot)=>{
+      const contactLists = snapshot.docs.map((doc)=>{
+          return{
+            id:doc.id,
+            ...doc.data()
+          }
+        })
+      const filteredContacts = contactLists.filter((contact)=>contact.name.toLowerCase().includes(value.toLowerCase()))
+          setContacts(filteredContacts)
+          return filteredContacts
+        })
+  }
+
   return (
-    <div className="mx-auto max-w-[370px]">
-        <Navbar/>
-      <div className="flex gap-2">
-        <div className="flex flex-grow relative items-center">
-          <FiSearch className="absolute ml-1 text-3xl text-white"/>
-          <input 
-          type="text"
-          className="h-10 flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
-          />
+    <>
+      <div className="mx-auto max-w-[370px]">
+          <Navbar/>
+        <div className="flex gap-2">
+          <div className="flex flex-grow relative items-center">
+            <FiSearch className="absolute ml-1 text-3xl text-white"/>
+            <input 
+            onChange={filterContacts}
+            type="text"
+            className="h-10 flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
+            />
+          </div>
+          <AiFillPlusCircle onClick={onOpen} className="text-5xl text-white cursor-pointer"/>
         </div>
-        <AiFillPlusCircle className="text-5xl text-white cursor-pointer"/>
+        <div className="mt-4 flex flex-col gap-3">
+          {contacts.map((contact)=>(
+            <ContactCard key={contact.id} contact={contact}/>
+          ))}
+        </div>
       </div>
-      <div className="mt-4 flex flex-col gap-3">
-        {contacts.map((contact)=>(
-          <ContactCard key={contact.id} contact={contact}/>
-        ))}
-      </div>
-    </div>
+      <AddAndUpdateContact
+        onClose={onClose}
+        isOpen={isOpen}
+      />
+    </>
   )
 
 }
--- Projects-101/05.Contact-App/src/components/ContactCard.jsx
+++ Projects-101/05.Contact-App/src/components/ContactCard.jsx
@@ -1,10 +1,27 @@
+import { deleteDoc, doc } from "firebase/firestore";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { IoMdTrash } from "react-icons/io";
 import { RiEditCircleLine } from "react-icons/ri";
+import { db } from "../config/firebase";
+import AddAndUpdateContact from "../AddAndUpdateContact";
+import useDisclouse from "../hooks/useDisclouse";
 
 export const ContactCard = ({contact}) => {
+  const {isOpen,onClose,onOpen} = useDisclouse()
+
+
+
+  const deleteContact = async (id) =>{
+    try {
+      await deleteDoc(doc(db,"contacts",id))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div key={contact.id} className="flex items-center justify-between rounded-lg bg-yellow-100 p-2">
+    <>
+      <div key={contact.id} className="flex items-center justify-between rounded-lg bg-yellow-100 p-2">
             <div className="flex gap-2">
             <HiOutlineUserCircle className="text-4xl text-orange-400"/>
             <div className="">
@@ -14,10 +31,16 @@ export const ContactCard = ({contact}) => {
             </div>
             
             <div className="flex text-3xl">
-              <RiEditCircleLine/>
-              <IoMdTrash className="text-orange-400"/>
+              <RiEditCircleLine 
+              onClick={onOpen}
+              className="cursor-pointer"/>
+              <IoMdTrash 
+              onClick={()=>deleteContact(contact.id)}
+              className="text-orange-400 cursor-pointer"/>
             </div>
           </div>
+          <AddAndUpdateContact contact={contact} isUpdate isOpen={isOpen} onClose={onClose}/>
+    </>
   )
 };
 
